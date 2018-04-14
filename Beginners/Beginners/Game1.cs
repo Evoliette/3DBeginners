0557858adda5275daa3b12babee4dee696cc4a93
evoliette <tessa.zander@st.ovgu.de>
2018-04-14 22:34:05 +0200

    Optimierung 2

--- Beginners/Beginners/Game1.cs
+++ Beginners/Beginners/Game1.cs
@@ -14,22 +14,22 @@ namespace Beginners
         const int max_models = 5;
         float radius = 2f;
         float range = 10;
-        int timer = 30000;
+        int timer;
 
-        //Camera
+        //camera
         Vector3 camPosition;
         Matrix projectionMatrix;
         Matrix viewMatrix;
         Matrix[] worldMatrix = new Matrix[max_models];
 
-        //Geometrics and Objects
+        //geometrics and objects
         Model[] model = new Model[max_models];
         Vector3[] modelPosition = new Vector3[max_models];
         Texture2D looser;
         Texture2D won;
         SpriteFont counter;
 
-        //Stuff
+        //stuff
         Random random = new Random();
         bool hit;
         bool gameOver;
@@ -45,7 +45,7 @@ namespace Beginners
         {
             base.Initialize();
 
-            //Setup Camera
+            //camera
             camPosition = new Vector3(0f, 15f, 10f);
             projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
                                MathHelper.ToRadians(45f), graphics.
@@ -54,7 +54,7 @@ namespace Beginners
             viewMatrix = Matrix.CreateLookAt(camPosition, new Vector3(0, 0, -5),
                          new Vector3(0f, 1f, 0f));// Y up
 
-            //Startpositions
+            //positions
             modelPosition[0] = new Vector3(0f, 0f, 0f);
 
             modelPosition[1] = new Vector3((float)random.NextDouble() * range - range, 0, -30f);
@@ -66,6 +66,9 @@ namespace Beginners
             {
             worldMatrix[i] = Matrix.CreateWorld(modelPosition[i], Vector3.Forward, Vector3.Up);
             }
+
+            //timer
+            timer = 30000;
         }
 
         protected override void LoadContent()
@@ -93,49 +96,55 @@ namespace Beginners
                 ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
                 Keys.Escape))
                 Exit();
+            //close game
 
-        if (!gameOver && !winner) { 
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            //gameplay
+            if (!gameOver && !winner)
             {
-                modelPosition[0].X -= 0.3f;
-                if(modelPosition[0].X < -range)
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Left))
                 {
-                    modelPosition[0].X = -range;
+                    modelPosition[0].X -= 0.3f;
+                    if (modelPosition[0].X < -range)
+                    {
+                        modelPosition[0].X = -range;
+                    }
                 }
-            }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                modelPosition[0].X += 0.3f;
-                if (modelPosition[0].X > range)
+                if (Keyboard.GetState().IsKeyDown(Keys.Right))
                 {
-                     modelPosition[0].X = range;
+                    modelPosition[0].X += 0.3f;
+                    if (modelPosition[0].X > range)
+                    {
+                        modelPosition[0].X = range;
+                    }
                 }
-            }
 
-            modelPosition[1].Z += 0.55f;
-            modelPosition[2].Z += 0.4f;
-            modelPosition[3].Z += 0.45f;
-            modelPosition[4].Z += 0.5f;
+                modelPosition[1].Z += 0.55f;
+                modelPosition[2].Z += 0.4f;
+                modelPosition[3].Z += 0.45f;
+                modelPosition[4].Z += 0.5f;
+                //speed of the bullets
 
-            for (int i = 1; i <= max_models-1; i++)
-            {
-                if (modelPosition[i].Z >= 1.5f)
+                for (int i = 1; i <= max_models - 1; i++)
                 {
-                    modelPosition[i] = new Vector3((float)random.NextDouble() * range * 2 - range, 0, -30f);
+                    if (modelPosition[i].Z >= 1.5f)
+                    {
+                        modelPosition[i] = new Vector3((float)random.NextDouble() * range * 2 - range, 0, -30f);
+                    }
                 }
-            }
-      
-            timer = timer - gameTime.ElapsedGameTime.Milliseconds;
+                //set back the bullets
 
-            if (timer == 0)
+                timer = timer - gameTime.ElapsedGameTime.Milliseconds;
+                if (timer == 0)
                 {
                     winner = true;
                 }
+                //timer
 
-            hit = false;
-
+                //collision
+                hit = false;
                 for (int i = 1; i <= max_models - 1; i++)
                 {
 
@@ -151,22 +160,21 @@ namespace Beginners
                 if (hit)
                 {
                     gameOver = true;
-                       
+
                 }
 
-         
-             //viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,Vector3.Up);
-             //camTarget = modelPosition;
-             //camera follows player
+                //viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,Vector3.Up);
+                //camTarget = modelPosition;
+                //camera follows player
 
-            for(int i=0; i < max_models; i++)
-            {
-                worldMatrix[i] = Matrix.CreateWorld(modelPosition[i], Vector3.Forward, Vector3.Up);
+                for (int i = 0; i < max_models; i++)
+                {
+                    worldMatrix[i] = Matrix.CreateWorld(modelPosition[i], Vector3.Forward, Vector3.Up);
+                }
+                //updates positions
             }
-             //aktualisiert die Figurpositionen
-        }
 
-        else if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            else if (Keyboard.GetState().IsKeyDown(Keys.Enter))
             {
                 for (int j = 1; j <= max_models - 1; j++)
                 {
@@ -174,7 +182,9 @@ namespace Beginners
                 }
                 gameOver = false;
                 winner = false;
+                timer = 30000;
             }
+            //restart
 
             base.Update(gameTime);
         }
@@ -190,17 +200,17 @@ namespace Beginners
                     foreach (BasicEffect effect in mesh.Effects)
                     {
                         effect.EnableDefaultLighting();
-                        effect.AmbientLightColor = new Vector3(1, 1, 1);    //Color of light
+                        effect.AmbientLightColor = new Vector3(1, 1, 1);    //color of light
                         effect.View = viewMatrix;
                         effect.World = worldMatrix[i];
                         effect.Projection = projectionMatrix;
                         if(i == 0)
                         {
-                            effect.DiffuseColor = new Vector3(.7f, .2f, 0); //Player = orange
+                            effect.DiffuseColor = new Vector3(.7f, .2f, 0); //player = orange
                         }
                         else 
                         {
-                            effect.DiffuseColor = new Vector3(.5f,.5f,.5f); //Others = white
+                            effect.DiffuseColor = new Vector3(.5f,.5f,.5f); //others = white
                         }
                     }
                     mesh.Draw();
@@ -208,13 +218,14 @@ namespace Beginners
             }
             spriteBatch.Begin();
             if (gameOver)
-            {         
+            {
                 spriteBatch.Draw(looser, new Rectangle(0,0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
+                
             }
-            if(winner)
-             { 
-                spriteBatch.Draw(won, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
-             }
+           if(winner)
+            {
+                spriteBatch.Draw(won, new Rectangle(100, 0, graphics.PreferredBackBufferWidth - 200, graphics.PreferredBackBufferHeight), Color.White);
+            }
 
         spriteBatch.DrawString(counter, timer.ToString(), new Vector2(0, 0), Color.White);
             spriteBatch.End();
